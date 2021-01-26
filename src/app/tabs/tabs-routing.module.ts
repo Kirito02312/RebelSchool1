5c877d068531b80c20667a0ca714c7426ee90040
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2021-01-25 21:43:28 -0300

    Aula02

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,11 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'user-add',
+        loadChildren: () => import('../pages/user-add/user-add.module').then( m => m.UserAddPageModule)
+      },
+      
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
