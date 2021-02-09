ace6ba135ac27e3737f69d5baa495426d187da99
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2021-02-08 21:47:22 -0300

    aula6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';   
+
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
+
+  
 })
 export class AppModule {}
--- src/app/pages/user-add/user-add.page.ts
+++ src/app/pages/user-add/user-add.page.ts
@@ -1,22 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
+import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-user-add',
   templateUrl: './user-add.page.html',
   styleUrls: ['./user-add.page.scss'],
 })
+
 export class UserAddPage implements OnInit {
 
   user : User = new User;
-  constructor() { }
+  constructor(private storage: Storage,
+  public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
   salvar(){
+    try{
     console.log('Dados Salvos...', this.user);
+    this.storage.set('nome', this.user.nome);
+    this.storage.set('email', this.user.email);
+    this.storage.set('senha', this.user.senha);
+    console.log('Dados salvos com sucesso...',this.user);
+    this.presentAlert(); 
+  } catch (error) {
+console.error("Erro ao salvar.", error);
+  }
+
 }
 
+async presentAlertConfirm() {
+  const alert = await this.alertController.create({
+    cssClass: 'my-custom-class',
+    header: 'alerta',
+    message: 'Usuário cadastrado com Sucesso!!!',
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (blah) => {
+          console.log('Confirm Cancel: blah');
+        }
+      }, {
+        text: 'Okay',
+        handler: () => {
+          console.log('Confirm Okay');
+        }
+      }
+    ]
+  });
+
+  await alert.present();
+}
 }
 
